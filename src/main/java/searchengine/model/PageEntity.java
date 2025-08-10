7d63ffd24f4941d7bc3b9885554b176912a369f5
gr3am <greampro@gmail.com>
2025-08-10 20:05:37 +0300

    Added Site & Page entities

---
+++ src/main/java/searchengine/model/PageEntity.java
@@ -0,0 +1,30 @@
+package searchengine.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "page", indexes = @Index(name = "idx_path", columnList = "path"))
+public class PageEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "site_id", nullable = false)
+    private SiteEntity site;
+
+    @Column(nullable = false)
+    private String path;
+
+    @Column(nullable = false)
+    private int code;
+
+    @Column(columnDefinition = "MEDIUMTEXT", nullable = false)
+    private String content;
+}
---
+++ src/main/java/searchengine/model/SiteEntity.java
@@ -0,0 +1,34 @@
+package searchengine.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "site")
+public class SiteEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    @Column(name = "status_time", nullable = false)
+    private LocalDateTime statusTime;
+
+    @Column(name = "last_error", columnDefinition = "TEXT")
+    private String lastError;
+
+    @Column(nullable = false)
+    private String url;
+
+    @Column(nullable = false)
+    private String name;
+}
